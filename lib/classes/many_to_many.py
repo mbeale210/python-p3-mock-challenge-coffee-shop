c948c46ce1f16073280061d58a152e1268c3a556
Mark Beale <bealebeaverst@gmail.com>
2024-08-29 22:43:20 -0700

    Complete Lab

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,91 @@
 class Coffee:
     def __init__(self, name):
-        self.name = name
-        
+        if not isinstance(name, str) or len(name) <= 2:
+            raise ValueError("Name must be a string longer than 2 characters")
+        self._name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        pass
+
     def orders(self):
-        pass
-    
+        return self._orders
+
     def customers(self):
-        pass
-    
+        return list(set(order.customer for order in self._orders))
+
     def num_orders(self):
-        pass
-    
+        return len(self._orders)
+
     def average_price(self):
-        pass
+        if not self._orders:
+            return 0
+        return sum(order.price for order in self._orders) / len(self._orders)
 
 class Customer:
     def __init__(self, name):
-        self.name = name
-        
+        self._name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 15:
+            self._name = value
+        # If the new value is invalid, we simply don't change the name
+
     def orders(self):
-        pass
-    
+        return self._orders
+
     def coffees(self):
-        pass
-    
+        return list(set(order.coffee for order in self._orders))
+
     def create_order(self, coffee, price):
-        pass
-    
+        order = Order(self, coffee, price)
+        return order
+
+
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
-        self.customer = customer
-        self.coffee = coffee
-        self.price = price
+        if not isinstance(customer, Customer):
+            raise TypeError("Customer must be an instance of Customer class")
+        if not isinstance(coffee, Coffee):
+            raise TypeError("Coffee must be an instance of Coffee class")
+        if not isinstance(price, float):
+            raise TypeError("Price must be a float")
+        if not (1.0 <= price <= 10.0):
+            raise ValueError("Price must be between 1.0 and 10.0")
+
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+        Order.all.append(self)
+        customer._orders.append(self)
+        coffee._orders.append(self)
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        # This setter does nothing, effectively making price immutable
+        pass
